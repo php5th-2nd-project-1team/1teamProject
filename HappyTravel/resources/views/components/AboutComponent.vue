ea80bf008e8481c93938abc56d787a0779f2327f
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2024-12-10 15:11:52 +0900

    241210 AboutComponent 추가

---
+++ HappyTravel/resources/views/components/AboutComponent.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="about-container">
+		<div class="about-introduce">
+			<p></p>
+		</div>
+	</div>
+</template>
+<script setup>
+
+</script>
+<style>
+	.about-component{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		gap:40px;
+
+		padding: 0 200px;
+	}
+</style>
