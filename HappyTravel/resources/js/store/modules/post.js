1264fd2771128fe0ae939898450a9b4148eab8e7
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2024-12-10 15:46:21 +0900

    241210 store module 추가

---
+++ HappyTravel/resources/js/store/modules/post.js
@@ -0,0 +1,15 @@
+export default {
+	namespaced: true,
+	state: () =>({
+		comment : {picture: '/img/abc.png', comment : '랄ㄹ랄ㄹ', name: '펫타곤', created_at: '2024-12-10'}
+	})
+	,mutations: {
+
+	}
+	,actions: {
+
+	}
+	,getters: {
+		
+	}
+}
