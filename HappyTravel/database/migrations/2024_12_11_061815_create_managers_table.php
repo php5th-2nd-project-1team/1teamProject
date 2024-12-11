7d5978b2c12411f9366dc617f54083f104760834
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2024-12-11 18:58:40 +0900

    241211 원상 마이그레이션 수정 및 적용

---
+++ HappyTravel/database/migrations/2024_12_11_061815_create_managers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('managers', function (Blueprint $table) {
+            $table->id('manager_id');
+            $table->string('m_account', 30)->nullable(false)->unique();
+            $table->string('m_password', 255)->nullable(false);
+            $table->string('m_nickname',30)->nullable(false);
+            $table->string('refresh_token',512)->nullable(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('managers');
+    }
+};
---
+++ HappyTravel/database/migrations/2024_12_11_073250_create_notices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->id('notice_id');
+            $table->unsignedBigInteger('manager_id')->nullable(false);
+            $table->string('notice_title', 50)->nullable(false);
+            $table->longText('notice_content')->nullable(false);
+            $table->string('notice_img', 255)->nullable(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+};
