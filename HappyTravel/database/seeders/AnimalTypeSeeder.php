507f2befbf266c57a6efe8437dbfbdd45ebf838b
woominju0208 <alswn5033@gmail.com>
2025-01-14 18:12:04 +0900

    250114 민주 포스트동물종류 시더

--- HappyTravel/database/seeders/AnimalTypeSeeder.php
+++ HappyTravel/database/seeders/AnimalTypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\AnimalType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +13,26 @@ class AnimalTypeSeeder extends Seeder
      *
      * @return void
      */
+    public function saveAnimalType($animal_type_name, $animal_type_num)
+    {
+        $animalType = new AnimalType();
+        $animalType->animal_type_name = $animal_type_name;
+        $animalType->animal_type_num = $animal_type_num;
+        $animalType->save();
+    }
+
+    public $data = [
+        '01' => '소형견'
+        ,'02' => '중형견'
+        ,'03' => '대형견'
+        ,'04' => '고양이'
+        ,'05' => '조류'
+    ];
+
     public function run()
     {
-        //
+        foreach($this->data as $key => $value){
+            $this->saveAnimalType($value, $key);
+        }
     }
 }
